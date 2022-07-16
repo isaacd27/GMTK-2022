4821af7c2a226b1def9d4bcf194d9f316dd577dd
Isaac Davis <72354474+isaacd27@users.noreply.github.com>
2022-07-16 10:32:45 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+ 
+
+    public float moveSpeed = 5f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+
+        // Debug.Log(IsGrounded());
+
+        // if (tilemap.ContainsTile(tilemap.name("Player")))
+        // {
+
+        //  }
+
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+}
+
+
+    
